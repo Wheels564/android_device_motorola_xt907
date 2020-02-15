1aa74a3cae9fc5e88aeaaf8238d011b40183d433
Wheels564 <53055870+Wheels564@users.noreply.github.com>
2020-02-15 10:13:48 -0500

    Add files via upload

---
+++ extract-files.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DEVICE=xt907
+export FAMILY_DEVICE=msm8960-common
+
+../$FAMILY_DEVICE/extract-files.sh $@
---
+++ lineage.mk
@@ -0,0 +1,22 @@
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/motorola/qcom-common/overlay-cm
+
+# Media
+PRODUCT_COPY_FILES += \
+    device/motorola/msm8960-common/config/media_codecs.xml:system/etc/media_codecs.xml
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 540
+
+PRODUCT_NAME := lineage_xt907
+TARGET_VENDOR_PRODUCT_NAME := XT907_verizon
+TARGET_VENDOR_DEVICE_NAME := scorpion_mini
+
+$(call inherit-product, device/motorola/xt907/full_xt907.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=motorola/XT907_verizon/scorpion_mini:4.4.2/KDA20.62-15.1/15:user/release-keys \
+    PRIVATE_BUILD_DESC="smq_vzw-user 4.4.2 KDA20.62-15.1 15 release-keys"
